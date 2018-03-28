2cf14bb4e66d24c770b61944c43c6b7b9494821f
tasuku <tasuku@gmail.com>
2018-03-28 15:09:07 +0900

    updated
    Signed-off-by: tasuku <tasuku@gmail.com>

--- amr_master/amr_nn_controller_service/scripts/models.py
+++ amr_master/amr_nn_controller_service/scripts/models.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models
 
+
 class ResNet18FT(nn.Module):
 
     """
@@ -24,35 +25,27 @@ class ResNet18FT(nn.Module):
         return x
 
 
-class CNNController(nn.Module):
+class ResNet18FE(nn.Module):
 
     """
-    Inspired by PilotNet per Nvidia paper.
-    
+    Feature Extract ResNet18
     """
+
     def __init__(self):
-        super(PilotNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 24, kernel_size=5, stride=2, padding=0)
-        self.conv2 = nn.Conv2d(24, 36, kernel_size=5, stride=2, padding=0)
-        self.conv3 = nn.Conv2d(36, 48, kernel_size=5, stride=2, padding=0)
-        self.conv4 = nn.Conv2d(48, 64, kernel_size=3, stride=1, padding=0)
-        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0)
-        self.fc1 = nn.Linear(64 * 1 * 18, 100)
-        self.fc2 = nn.Linear(100, 50)
-        self.fc3 = nn.Linear(50, 10)
-        self.fc4 = nn.Linear(10, 2)
+        super(ResNet18FE, self).__init__()
+        self.resnet18fe = models.resnet18(pretrained=True)
+        # Set grad to false to freeze.
+        for param in self.resnet18fe.parameters():
+            param.requires_grad = False
+
+        # Default sets requires_grad to false,
+        # so final fc can be optimized.
+        num_ftrs = self.resnet18fe.fc.in_features
+        self.resnet18fe.fc = nn.Linear(num_ftrs, 2)
+        self.resnet18fe.cuda()
 
     def forward(self, x):
-        x = (x - 0.5) * 2.0
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = F.relu(self.conv5(x))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.resnet18fe(x)
         return x
+
 
